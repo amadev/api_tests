b9590d66d7c75934188b78a56b6466f0a67b2f24
Andrey Volkov <avolkov@mirantis.com>
2018-05-29 16:04:09 +0300

    Common client concept

--- creds.py
+++ creds.py
@@ -6,27 +6,85 @@ from requests_toolbelt.utils import dump
 HOST = 'james'
 
 CINDER_URL = 'http://%s:8776/v3/' % HOST
-NOVA_URL = 'http://%s:8774/v2.1/' % HOST
+NOVA_URL = 'http://%s/compute/v2.1/' % HOST
 AUTH_URL = 'http://%s/identity/v2.0/tokens' % HOST
 PL_URL = 'http://%s/placement/' % HOST
 
 USER = 'admin'
 PASS = 'admin'
 TENANT = 'admin'
-DEMO_USER = '142c8f51c05a4e9592f050d6d82e1f72'
-DEMO_TENANT = 'dd4b5e19a0e74bfb8a043744c6bf4318'
-ADMIN_TENANT = '26e9ece758dd449b96fff2e65fb48299'
+DEMO_USER = 'demo'
+DEMO_PASS = 'admin'
+DEMO_TENANT = 'demo'
+TENANT_ID = '475bd22e5d3949a08a59a9fdb19850f5'
+DEMO_TENANT_ID = '7b45e6344a874fb8986294a255aea17c'
 
 
-def headers():
+def headers(user=None, pswd=None, tenant=None):
+    user = user or USER
+    pswd = pswd or PASS
+    tenant = tenant or TENANT
     headers = {'content-type': 'application/json'}
     payload = {"auth":
-               {"tenantName": TENANT,
+               {"tenantName": tenant,
                 "passwordCredentials":
-                {"username": USER, "password": PASS}}}
+                {"username": user, "password": pswd}}}
     r = requests.post(AUTH_URL,
                       data=json.dumps(payload),
                       headers=headers)
     print dump.dump_all(r)
     headers['x-auth-token'] = r.json()['access']['token']['id']
     return headers
+
+
+class Client(object):
+
+    def __init__(self, url=PL_URL[0:-1], keywords=None, convert_names=False,
+                 user=None, pswd=None, tenant=None):
+        self.url = url
+        self.keywords = keywords or []
+        self.convert_names = convert_names
+        self.user = user
+        self.pswd = pswd
+        self.tenant = tenant
+
+    def __getattr__(self, name):
+        if self.convert_names:
+            name = name.replace('_', '-')
+        if name in ['get', 'post', 'put', 'delete']:
+            return lambda *args, **kwargs: self.call(name, *args, **kwargs)
+        elif not self.keywords or name in self.keywords:
+            return Client(self.url + '/' + name,
+                          keywords=self.keywords,
+                          convert_names=self.convert_names,
+                          user=self.user,
+                          pswd=self.pswd,
+                          tenant=self.tenant)
+        else:
+            raise RuntimeError('unknown keyword %s' % name)
+
+    def __call__(self, *args):
+        params = [self.url]
+        params.extend(args)
+        return Client('/'.join(params),
+                      keywords=self.keywords,
+                      convert_names=self.convert_names,
+                      user=self.user,
+                      pswd=self.pswd,
+                      tenant=self.tenant)
+
+    def call(self, method, *args, **kwargs):
+        _headers = headers(self.user, self.pswd, self.tenant)
+        print 'calling url %s for user  %s' % (self.url, self.user)
+        status_code = kwargs.pop('status_code', 200)
+        version = kwargs.pop('version', None)
+        if version:
+            _headers.update(
+                {'OpenStack-API-Version': 'placement %s' % version})
+        r = getattr(requests, method)(
+            self.url, headers=_headers, *args, **kwargs)
+        print dump.dump_all(r)
+        assert status_code == r.status_code, r.text
+        if not r.text:
+            return {}
+        return r.json()
--- test_nova_quota.py
+++ test_nova_quota.py
@@ -3,9 +3,27 @@ import requests
 import json
 from creds import *
 
-def test_show_quotas_for_user():
-    _headers = headers()
-    r = requests.get(
-        NOVA_URL + 'os-quota-sets/%s?user_id=1&user_id=2' % DEMO_TENANT,
-        headers=_headers)
-    assert 200 == r.status_code, r.text
+# C = Client(
+#     NOVA_URL[0:-1],
+#     convert_names=True)
+
+
+# def test_show_quotas_for_user():
+#     _headers = headers()
+#     r = requests.get(
+#         NOVA_URL + 'os-quota-sets/%s?user_id=1&user_id=2' % DEMO_TENANT_ID,
+#         headers=_headers)
+#     assert 200 == r.status_code, r.text
+
+
+def test_get_quota_details():
+    # C.os_quota_sets(DEMO_TENANT_ID).detail().get()
+    c = Client(
+        NOVA_URL[0:-1],
+        convert_names=True,
+        user=DEMO_USER,
+        pswd=DEMO_PASS,
+        tenant=DEMO_TENANT)
+    # c.os_quota_sets(DEMO_TENANT_ID).detail().get(status_code=403)
+    # C.limits().get()
+    c.limits().get()
